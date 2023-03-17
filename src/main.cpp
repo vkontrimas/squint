871357fc81014a6782615f5ed123891fab9b27ee
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-17 13:48:16 +0000

    open display

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
-int main(int, char**) { return 0; }
+#include <cassert>
+#include <cstdlib>
+#include <X11/Xlib.h>
+
+namespace {
+  const char* getDisplay() {
+    constexpr const char* kDefaultDisplay = ":0";
+    const auto display = getenv("DISPLAY");
+    return display ? display : kDefaultDisplay;
+  }
+}
+
+int main(int, char**) { 
+  Display* display = XOpenDisplay(getDisplay());
+  assert(display);
+
+  return 0; 
+}
