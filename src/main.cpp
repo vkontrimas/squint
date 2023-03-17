509503cc994f75dff4781dd0e58c865ddd8224a5
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-17 15:18:08 +0000

    Take a fullscreen screenshot!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <cassert>
 #include <cstdlib>
 #include <memory>
+#include <fstream>
 #include <X11/Xlib.h>
+#include <X11/extensions/Xrender.h>
 
 namespace {
   const char* getDisplay() {
@@ -11,17 +13,54 @@ namespace {
   }
 }
 
+struct Rect {
+  int x, y, width, height;
+};
+
 struct DisplayDeleter {
   void operator()(Display* display) {
     XCloseDisplay(display);
   }
 };
-
 using DisplayPtr = std::unique_ptr<Display, DisplayDeleter>;
 
 int main(int, char**) { 
   DisplayPtr display{XOpenDisplay(getDisplay())};
   assert(display);
 
+  Window rootWindow = XDefaultRootWindow(display.get());
+  assert(rootWindow);
+
+  XWindowAttributes rootAttribs;
+  XGetWindowAttributes(display.get(), rootWindow, &rootAttribs);
+
+  XRenderPictFormat* sourceFormat = XRenderFindVisualFormat(display.get(), rootAttribs.visual);
+  XRenderPictureAttributes sourceAttribs;
+  sourceAttribs.subwindow_mode = IncludeInferiors; // Not entirely sure what this does
+
+  Picture source = XRenderCreatePicture(display.get(), rootWindow, sourceFormat, CPSubwindowMode, &sourceAttribs);
+  assert(source);
+
+  Pixmap pixmap = XCreatePixmap(display.get(), rootWindow, rootAttribs.width, rootAttribs.height, 32);
+  assert(pixmap);
+
+  // TODO: check if destinationFormat matches what i3 expects
+  XRenderPictFormat* destinationFormat = XRenderFindStandardFormat(display.get(), PictStandardARGB32);
+  XRenderPictureAttributes destinationAttribs;
+  Picture destination = XRenderCreatePicture(display.get(), pixmap, destinationFormat, 0, &destinationAttribs);
+  assert(destination);
+
+  XRenderColor black { 0x0000, 0x0000, 0x0000, 0x0000 };
+  XRenderFillRectangle(display.get(), PictOpSrc, destination, &black, 0, 0, rootAttribs.width, rootAttribs.height);
+  XRenderComposite(display.get(), PictOpSrc, source, 0, destination, 0, 0, 0, 0, 0, 0, rootAttribs.width, rootAttribs.height);
+
+  XImage* output = XGetImage(display.get(), pixmap, 0, 0, rootAttribs.width, rootAttribs.height, AllPlanes, ZPixmap);
+  assert(output);
+
+  std::fstream file {"test.raw", std::ios::binary | std::ios::out};
+  file.write(output->data, output->width * output->height * 4);
+
+  // TODO: Find appropriate place to free this
+  XFreePixmap(display.get(), pixmap);
   return 0; 
 }
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+build/squint
+convert -size 3840x2160 -depth 8 BGRA:test.raw -separate -delete 3 -combine test.png
+feh --fullscreen test.png
+rm -f test.raw test.png
