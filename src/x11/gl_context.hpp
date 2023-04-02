6ca74d771dddaa18bc332bee89b5a193f8dad50b
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-02 15:42:08 +0100

    Fix up GLContext

--- src/x11/gl_context.hpp
+++ src/x11/gl_context.hpp
@@ -13,23 +13,24 @@ namespace squint::x11 {
     Colormap colorMap;
     Window window;
 
-    GLContext(Display* display, void* context, Colormap colorMap, Window window) 
+    GLContext(Display* display, void* context, Colormap colorMap, Window window) noexcept
       : display{display}, context{context}, colorMap{colorMap}, window{window} {}
     ~GLContext();
 
     GLContext(const GLContext& other) = delete;
     GLContext& operator=(const GLContext& other) = delete;
 
-    GLContext(GLContext&& other) {
+    GLContext(GLContext&& other) noexcept {
       std::swap(*this, other);
     }
 
-    GLContext& operator=(GLContext&& other) {
+    GLContext& operator=(GLContext&& other) noexcept {
       std::swap(*this, other);
       return *this;
     }
 
-    friend void swap(GLContext& first, GLContext& second) {
+    friend void swap(GLContext& first, GLContext& second) noexcept {
+      using std::swap;
       std::swap(first.display, second.display);
       std::swap(first.context, second.context);
       std::swap(first.colorMap, second.colorMap);
