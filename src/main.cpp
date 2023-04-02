9e82132da2cfcbc6bb679088a1547b981828b407
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-02 13:15:53 +0100

    Extract GL context creation into lambda

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,9 @@
 #include <squint/glsl/test.vert.h>
 
 namespace {
-  const char* getDisplay() {
-    constexpr const char* kDefaultDisplay = ":0";
-    const auto display = getenv("DISPLAY");
-    return display ? display : kDefaultDisplay;
-  }
-
   void openglErrorCallback(GLenum, GLenum type, GLuint, GLenum severity, GLsizei, const GLchar* message, const void*) {
-      fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ), type, severity, message);
-    }
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ), type, severity, message);
+  }
 }
 
 struct DisplayDeleter {
@@ -41,9 +35,17 @@ int customXErrorHandlerForGLInit(Display*, XErrorEvent*) {
 
 int main(int, char**) { 
   // 🤢 this is ugly code, pls ignore
+  const auto openDisplay = []() {
+    const char* displayId = getenv("DISPLAY");
+    if (!displayId) {
+      displayId = ":0";
+    }
 
-  DisplayPtr display{XOpenDisplay(getDisplay())};
-  assert(display);
+    DisplayPtr display{XOpenDisplay(displayId)};
+    assert(display);
+    return display;
+  };
+  const auto display = openDisplay();
 
   /*
    * X11 screenshot experiment
@@ -84,119 +86,126 @@ int main(int, char**) {
   /*
    * OPENGL CONTEXT EXPERIMENT
    */
-  // Verify GLX version
-  int glxMajor, glxMinor;
-  auto success = glXQueryVersion(display.get(), &glxMajor, &glxMinor);
-  assert(success); assert(glxMajor >= 1 && glxMinor >= 3);
+  const auto initializeGLContext = [&display]() {
+    // Verify GLX version
+    int glxMajor, glxMinor;
+    auto success = glXQueryVersion(display.get(), &glxMajor, &glxMinor);
+    assert(success); assert(glxMajor >= 1 && glxMinor >= 3);
 
-  // Grab framebuffer config info
-  const static int visualAttributes[] = {
-    GLX_X_RENDERABLE, True,
-    GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
-    GLX_RENDER_TYPE, GLX_RGBA_BIT,
-    GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
-    // TODO: allowing for float RGB would allow more precision before we downrender to final image
-    GLX_RED_SIZE, 8,
-    GLX_GREEN_SIZE, 8,
-    GLX_BLUE_SIZE, 8,
-    GLX_ALPHA_SIZE, 8,
-    GLX_DEPTH_SIZE, 24, // TODO: technically we don't care about the depth size
-    GLX_STENCIL_SIZE, 8, // TODO: nor do we care about stencil size?
-    GLX_DOUBLEBUFFER, True, // TODO: nor double buffer (we draw once?)
-    // TODO: maybe we just enable these?
-    // GLX_SAMPLE_BUFFERS, 1,
-    // GLX_SAMPLES, 4,
-    None
-  };
+    // Grab framebuffer config info
+    const static int visualAttributes[] = {
+      GLX_X_RENDERABLE, True,
+      GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
+      GLX_RENDER_TYPE, GLX_RGBA_BIT,
+      GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
+      // TODO: allowing for float RGB would allow more precision before we downrender to final image
+      GLX_RED_SIZE, 8,
+      GLX_GREEN_SIZE, 8,
+      GLX_BLUE_SIZE, 8,
+      GLX_ALPHA_SIZE, 8,
+      GLX_DEPTH_SIZE, 24, // TODO: technically we don't care about the depth size
+      GLX_STENCIL_SIZE, 8, // TODO: nor do we care about stencil size?
+      GLX_DOUBLEBUFFER, True, // TODO: nor double buffer (we draw once?)
+      // TODO: maybe we just enable these?
+      // GLX_SAMPLE_BUFFERS, 1,
+      // GLX_SAMPLES, 4,
+      None
+    };
 
-  int framebufferConfigCount;
-  GLXFBConfig* framebufferConfigs = glXChooseFBConfig(
-    display.get(), DefaultScreen(display.get()), visualAttributes, &framebufferConfigCount
-  ); 
-  assert(framebufferConfigs);
+    int framebufferConfigCount;
+    GLXFBConfig* framebufferConfigs = glXChooseFBConfig(
+        display.get(), DefaultScreen(display.get()), visualAttributes, &framebufferConfigCount
+        ); 
+    assert(framebufferConfigs);
 
-  int bestIndex = -1;
-  int bestSamples = -1;
-  for (int i = 0; i < framebufferConfigCount; ++i) {
-    XVisualInfo* visualInfo = glXGetVisualFromFBConfig(display.get(), framebufferConfigs[i]);
+    int bestIndex = -1;
+    int bestSamples = -1;
+    for (int i = 0; i < framebufferConfigCount; ++i) {
+      XVisualInfo* visualInfo = glXGetVisualFromFBConfig(display.get(), framebufferConfigs[i]);
 
-    if (visualInfo) {
-      int sampleBuffers;
-      glXGetFBConfigAttrib(display.get(), framebufferConfigs[i], GLX_SAMPLE_BUFFERS, &sampleBuffers);
-      int samples;
-      glXGetFBConfigAttrib(display.get(), framebufferConfigs[i], GLX_SAMPLES, &samples);
-      if (bestIndex  == -1 || (sampleBuffers && samples > bestSamples)) {
-        bestIndex = i;
-        bestSamples = samples;
+      if (visualInfo) {
+        int sampleBuffers;
+        glXGetFBConfigAttrib(display.get(), framebufferConfigs[i], GLX_SAMPLE_BUFFERS, &sampleBuffers);
+        int samples;
+        glXGetFBConfigAttrib(display.get(), framebufferConfigs[i], GLX_SAMPLES, &samples);
+        if (bestIndex  == -1 || (sampleBuffers && samples > bestSamples)) {
+          bestIndex = i;
+          bestSamples = samples;
+        }
       }
+
+      XFree(visualInfo);
     }
+    assert(bestIndex > -1);
+    assert(bestSamples > -1);
+    auto framebufferConfig = framebufferConfigs[bestIndex];
+    XFree(framebufferConfigs);
 
+    // Get visual
+    XVisualInfo* visualInfo = glXGetVisualFromFBConfig(display.get(), framebufferConfig);
+    Colormap colorMap = XCreateColormap(
+        display.get(), RootWindow(display.get(), visualInfo->screen), visualInfo->visual, AllocNone
+        );
+    XSetWindowAttributes windowAttribs;
+    windowAttribs.colormap = colorMap;
+    windowAttribs.background_pixmap = None;
+    windowAttribs.border_pixel = 0;
+    windowAttribs.event_mask = StructureNotifyMask;
+    Window window = XCreateWindow(
+        display.get(),
+        RootWindow(display.get(), visualInfo->screen),
+        0, 0, 1, 1, 0, visualInfo->depth, InputOutput,
+        visualInfo->visual,
+        CWBorderPixel | CWColormap | CWEventMask, &windowAttribs
+        );
     XFree(visualInfo);
-  }
-  assert(bestIndex > -1);
-  assert(bestSamples > -1);
-  auto framebufferConfig = framebufferConfigs[bestIndex];
-  XFree(framebufferConfigs);
+    assert(window);
+    XStoreName(display.get(), window, "foo");
 
-  // Get visual
-  XVisualInfo* visualInfo = glXGetVisualFromFBConfig(display.get(), framebufferConfig);
-  Colormap colorMap = XCreateColormap(
-    display.get(), RootWindow(display.get(), visualInfo->screen), visualInfo->visual, AllocNone
-  );
-  XSetWindowAttributes windowAttribs;
-  windowAttribs.colormap = colorMap;
-  windowAttribs.background_pixmap = None;
-  windowAttribs.border_pixel = 0;
-  windowAttribs.event_mask = StructureNotifyMask;
-  Window window = XCreateWindow(
-    display.get(),
-    RootWindow(display.get(), visualInfo->screen),
-    0, 0, 1, 1, 0, visualInfo->depth, InputOutput,
-    visualInfo->visual,
-    CWBorderPixel | CWColormap | CWEventMask, &windowAttribs
-  );
-  XFree(visualInfo);
-  assert(window);
-  XStoreName(display.get(), window, "foo");
+    using glXCreateContextAttribsARBProc = GLXContext(Display*, GLXFBConfig, GLXContext, Bool, const int*);
+    glXCreateContextAttribsARBProc* glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc*)glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
 
-  using glXCreateContextAttribsARBProc = GLXContext(Display*, GLXFBConfig, GLXContext, Bool, const int*);
-  glXCreateContextAttribsARBProc* glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc*)glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
+    contextCreationError = false;
+    auto oldErrorHandler = XSetErrorHandler(&customXErrorHandlerForGLInit);
 
-  contextCreationError = false;
-  auto oldErrorHandler = XSetErrorHandler(&customXErrorHandlerForGLInit);
+    // TODO: Robustness. For now assuming we're creating GL 3.0 context
+    int contextAttributes[] = {
+      GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+      GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+      None
+    };
+
+    GLXContext glContext = glXCreateContextAttribsARB(
+        display.get(),
+        framebufferConfig,
+        0,
+        True,
+        contextAttributes
+        );
 
 
-  // TODO: Robustness. For now assuming we're creating GL 3.0 context
-  int contextAttributes[] = {
-    GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
-    GLX_CONTEXT_MINOR_VERSION_ARB, 0,
-    None
+    XSync(display.get(), False);
+    assert(!contextCreationError && glContext);
+    XSetErrorHandler(oldErrorHandler);
+
+    assert(glXIsDirect(display.get(), glContext));
+
+    // Make context current
+    glXMakeCurrent(display.get(), window, glContext);
+
+    // Load the rest of GL
+    squint::loadGL((squint::GetProcAddressFuncT*)glXGetProcAddressARB);
+
+    // Set up error callback
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(openglErrorCallback, 0);
+
+    // glXMakeCurrent(display.get(), 0, nullptr);
+    // glXDestroyContext(display.get(), glContext);
+    // XDestroyWindow(display.get(), window);
+    // XFreeColormap(display.get(), colorMap);
   };
-
-  GLXContext glContext = glXCreateContextAttribsARB(
-    display.get(),
-    framebufferConfig,
-    0,
-    True,
-    contextAttributes
-  );
-
-
-  XSync(display.get(), False);
-  assert(!contextCreationError && glContext);
-  XSetErrorHandler(oldErrorHandler);
-
-  assert(glXIsDirect(display.get(), glContext));
-
-  // Make context current
-  glXMakeCurrent(display.get(), window, glContext);
-
-  // Load the rest of GL
-  squint::loadGL((squint::GetProcAddressFuncT*)glXGetProcAddressARB);
-
-  // Set up error callback
-  glEnable(GL_DEBUG_OUTPUT);
-  glDebugMessageCallback(openglErrorCallback, 0);
+  initializeGLContext();
 
   // Set up framebuffer
   std::cout << output->width << " " << output->height << std::endl;
@@ -331,11 +340,6 @@ int main(int, char**) {
   glDeleteProgram(program);
   glDeleteShader(fragmentShader);
   glDeleteShader(vertexShader);
-
-  glXMakeCurrent(display.get(), 0, nullptr);
-  glXDestroyContext(display.get(), glContext);
-  XDestroyWindow(display.get(), window);
-  XFreeColormap(display.get(), colorMap);
 
   return 0; 
 }
