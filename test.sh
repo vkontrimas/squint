e716dc1f299a22aabe60a5d728a610fc9971c0ca
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-02 23:14:28 +0100

    only run test if build succeeds

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cmake --build build
-build/squint
-convert -size 3840x2160 -depth 8 BGRA:test.raw -separate -delete 3 -combine test.png
-feh --fullscreen test.png
-rm -f test.raw test.png
+if cmake --build build; then
+  build/squint
+  convert -size 3840x2160 -depth 8 BGRA:test.raw -separate -delete 3 -combine test.png
+  feh --fullscreen test.png
+  rm -f test.raw test.png
+fi
