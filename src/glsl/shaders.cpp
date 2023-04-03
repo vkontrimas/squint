a8f7e826113ee39a9ce689b80874540c7ed2a8d8
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-03 12:07:34 +0100

    glsl: create average and upscale shaders

--- src/glsl/shaders.cpp
+++ src/glsl/shaders.cpp
@@ -1,2 +1,4 @@
 #include <squint/glsl/fast_pixelate.frag.h>
 #include <squint/glsl/quad.vert.h>
+#include <squint/glsl/upscale.frag.h>
+#include <squint/glsl/average.frag.h>
---
+++ src/glsl/upscale.frag
@@ -0,0 +1,11 @@
+#version 330
+
+uniform sampler2D screen;
+
+in vec2 fragTexCoord;
+
+layout (location = 0) out vec4 result;
+
+void main() {
+  result = vec4(1.0);
+}
